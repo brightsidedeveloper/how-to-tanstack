c1f9a342ef7995702ef26d37b0c5667910a4f786
tim <tim@brightsidedeveloper.com>
2024-09-25 18:26:21 -0500

    Oh Yeah! I'm a commit message!

--- web/src/components/Branding.tsx
+++ web/src/components/Branding.tsx
@@ -8,13 +8,13 @@ export default function Branding() {
         <p className="text-xs @md:text-sm @lg:text-base text-center">There are a lot of things you can do with TanStack.</p>
         <span className="text-xs @md:text-sm @lg:text-base flex items-center justify-center">
           <Tooltip content="Wow, tooltip support!">
-            <a href="https://brightside-developer-docs.vercel.app" className="text-primary underline mr-5">
-              BrightSide API Docs
+            <a href="https://github.com/brightsidedeveloper" className="text-primary underline mr-5">
+              Follow Me On Github
             </a>
           </Tooltip>
           <Tooltip content="Wow, tooltip support!">
-            <a href="https://github.com/brightsidedeveloper/brightside-developer" className="text-primary underline">
-              BrightSide API Github
+            <a href="https://github.com/brightsidedeveloper/how-to-tanstack" className="text-primary underline">
+              Clone this Repo
             </a>
           </Tooltip>
         </span>
